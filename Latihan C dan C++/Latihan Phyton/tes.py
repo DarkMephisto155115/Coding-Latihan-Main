5530212f22bc010afd9c42e6ad14fbdc1b50cd2a
Andika Salsabilah <andika.as995@gmail.com>
2022-10-16 21:30:28 +0700

    op

--- Latihan C dan C++/Latihan Phyton/tes.py
+++ Latihan C dan C++/Latihan Phyton/tes.py
@@ -1,1 +1,8 @@
-print("Hello World")
+a = 237
+b = 58 / 60
+c = 43 / 3600
+print(a)
+print(b)
+print(c)
+hasil = a + b + c
+print(hasil)
---
+++ Latihan C dan C++/q2eqe2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    double hasil_b;
+    double hasil_c;
+    hasil_b = (58 / 60);
+    hasil_c = (43 / 3600);
+    printf("%lf \n", hasil_b);
+    printf("%lf \n", hasil_c);
+    return 0;
+}
