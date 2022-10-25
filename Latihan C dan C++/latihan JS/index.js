3f5d99df0a6c2c9efbf239c87cc893e040143fd6
Andika Salsabilah <andika.as995@gmail.com>
2022-10-26 00:37:35 +0700

    op

--- Latihan C dan C++/latihan JS/index.js
+++ Latihan C dan C++/latihan JS/index.js
@@ -1,3 +1,6 @@
-anjay_mabar(){
-    document.getElementById("tes")="wkwkwkwwkkwkw";
+let array = ["wwkwkwkkwkwkw", "wefewfwefwfewf", "awdwadaerverv", "opopopoapwdopaod", 23, 99, 1.23, "70%", "d1d12d12d"];
+let count = -1;
+function anjay_mabar(){
+    count += 1
+    document.getElementById("tes").textContent = array[count];
 }
