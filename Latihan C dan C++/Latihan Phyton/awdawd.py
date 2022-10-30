87945c6d78212af4fd21681fda2fad3dce04e9a6
Andika Salsabilah <andika.as995@gmail.com>
2022-10-30 23:05:06 +0700

    op

---
+++ Latihan C dan C++/Latihan Phyton/awdawd.py
@@ -0,0 +1,7 @@
+print("program mendeteksi bilangan ganjil dan genap")
+angka = int(input("masukkan angka : "))
+if (angka % 2 == 0):
+    print(angka, "adalah bilangan genap")
+else:
+    print(angka, "adalah bilangan ganjil")
+input()
--- Latihan C dan C++/latihan JS/index.js
+++ Latihan C dan C++/latihan JS/index.js
@@ -1,6 +1,31 @@
-let array = ["wwkwkwkkwkwkw", "wefewfwefwfewf", "awdwadaerverv", "opopopoapwdopaod", 23, 99, 1.23, "70%", "d1d12d12d"];
-let count = -1;
-function anjay_mabar(){
-    count += 1
-    document.getElementById("tes").textContent = array[count];
+function genap_ganjil(){
+    var inputEl = document.getElementById("input-el").value;
+    console.log(inputEl)
+    if ((inputEl%2) == 0){ // Genap
+        console.log("Genap")
+        document.getElementById("display-hasil").innerText = "Genap"
+        
+    }else if ((inputEl%2) == 1){ // Ganjil
+        console.log("Ganjil")
+        document.getElementById("display-hasil").innerText = "Ganjil"
+    }
+
+}
+
+function index_nilai(){
+    var inputEl_nilai = document.getElementById("input-el-nilai").value;
+    if (inputEl_nilai >= 80 && inputEl_nilai <= 100){ 
+        document.getElementById("display-hasil-nilai").innerText = "A"}
+    else if (inputEl_nilai >= 70 && inputEl_nilai <= 79){ 
+        document.getElementById("display-hasil-nilai").innerText = "AB"}
+    else if (inputEl_nilai >= 60 && inputEl_nilai <= 69){ 
+        document.getElementById("display-hasil-nilai").innerText = "B"}
+    else if (inputEl_nilai >= 50 && inputEl_nilai <= 59){ 
+        document.getElementById("display-hasil-nilai").innerText = "BC"}
+    else if (inputEl_nilai >= 40 && inputEl_nilai <= 49){ 
+        document.getElementById("display-hasil-nilai").innerText = "C"}
+    else if (inputEl_nilai >= 30 && inputEl_nilai <= 39){ 
+        document.getElementById("display-hasil-nilai").innerText = "D"}
+    else if (inputEl_nilai >= 0 && inputEl_nilai <= 29){ 
+        document.getElementById("display-hasil-nilai").innerText = "E"}
 }
