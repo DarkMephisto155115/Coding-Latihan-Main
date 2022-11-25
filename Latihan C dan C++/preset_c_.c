99378700b768d0eadd4fbff50b19f280cdb591e8
Andika Salsabilah <andika.as995@gmail.com>
2022-11-25 16:45:50 +0700

    op

--- Latihan C dan C++/preset_c_.c
+++ Latihan C dan C++/preset_c_.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+    int x = 0;
+    char i;
+    for ( i ; i != 'n';)
+    {
+        fflush(stdin);
+        scanf("%c", &i);
+        printf("ANJAY MABAR\n");
+    }
 
     system("pause");
     return 0;
