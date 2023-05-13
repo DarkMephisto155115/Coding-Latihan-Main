3eeb675d434219df3ca40397db2046ad642cda8e
Andika Salsabilah <andika.as995@gmail.com>
2023-05-13 14:22:22 +0700

    getLost

--- Java/testing/tes.java
+++
@@ -1,9 +0,0 @@
-package Java.testing;
-
-public class tes {
-    public static void main(String[] args) {
-        int health = 100;
-        int damage = (int) (health * 0.98);
-        System.out.println(damage);
-    }
-}
