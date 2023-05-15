b962cf18e4654112ae96635f92ab3d2a341483f1
Andika Salsabilah <andika.as995@gmail.com>
2023-05-15 18:13:12 +0700

    op

---
+++ Java/Latihan/tes1.java
@@ -0,0 +1,7 @@
+package Java.Latihan;
+
+public class tes1 {
+    public static void main(String[] args) {
+
+    }
+}
--- Java/Praktikum4/Demo/Membership/Membership.java
+++ Java/Praktikum4/Demo/Membership/Membership.java
@@ -5,7 +5,5 @@ public interface Membership {
     public double GOLD_DISC = 0.075;
     public double SILVER_DISC = 0.05;
 
-    public default double getMembershipDiscount() {
-        return 0;
-    }
+    public double getMembershipDiscount();
 }
